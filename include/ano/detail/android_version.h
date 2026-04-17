0a8339d467e32ac4199347be4e5f6f3d4300d0a0
FullError11 <none>
2026-04-17 23:20:49 +0800

    优化性能

--- include/ano/detail/android_version.h
+++ include/ano/detail/android_version.h
@@ -1,16 +1,25 @@
 #pragma once
 
+#include <stdexcept>
 #include <sys/system_properties.h>
+
+#include "error_quit.h"
 
 namespace ano::detail {
 
 inline unsigned int getAndroidVersion() {
-    char release_version[PROP_VALUE_MAX];
-    __system_property_get("ro.build.version.release", release_version);
-    try {
-        return atoi(release_version);
-    } catch (...) {}
-    return 0;
+
+    static unsigned int cache = []{
+        char release_version[PROP_VALUE_MAX];
+        __system_property_get("ro.build.version.release", release_version);
+        try {
+            return atoi(release_version);
+        } catch (...) {
+            errorQuit<std::runtime_error>("获取系统安卓版本失败");
+        }
+        return 0;  // never
+    }();
+    return cache;
 }
 
 }
